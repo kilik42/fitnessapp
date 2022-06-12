711fd1e9e65e6c7214e71b9081146375db41d89b
kilik42 <marvinevins@yahoo.com>
2022-06-11 20:42:54 -0500

    navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,8 +16,10 @@ const Navbar = () => {
       mt:{
         sm: '32px', 
         xs: '20px'
-      }
+      },
+       justifyContent: 'none'
     }}
+    px="20px"
     >
       <Link to="/">
         <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin: '0 20px' }} />
